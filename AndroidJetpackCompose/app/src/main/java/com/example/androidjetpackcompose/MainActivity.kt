dfbea9636b214d7aa0b4797ae1509041474664d3
choi minjun <>
2021-07-20 14:25:50 +0900

    study edittext

--- AndroidJetpackCompose/app/src/main/java/com/example/androidjetpackcompose/MainActivity.kt
+++ AndroidJetpackCompose/app/src/main/java/com/example/androidjetpackcompose/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.androidjetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidjetpackcompose.ui.theme.AndroidJetpackComposeTheme
+import kotlin.reflect.typeOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +26,51 @@ class MainActivity : ComponentActivity() {
             AndroidJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    SimpleFilledTextFieldSample()
                 }
             }
         }
     }
+}
+@Composable
+fun OverflowedText() {
+    Text("Hello Compose ".repeat(50), maxLines = 2, overflow = TextOverflow.Ellipsis)
+}
+@Composable
+fun PartiallySelectableText() {
+    SelectionContainer {
+        Column {
+            Text("This text is selectable")
+            Text("This one too")
+            Text("This one as well")
+            DisableSelection {
+                Text("But not this one")
+                Text("Neither this one")
+            }
+            Text("But again, you can select this one")
+            Text("And this one too")
+        }
+    }
+}
+
+@Composable
+fun SimpleClickableText() {
+    ClickableText(
+        text = AnnotatedString("Click Me"),
+        onClick = { offset ->
+            println("click $offset")
+        }
+    )
+}
+@Composable
+fun SimpleFilledTextFieldSample() {
+    var text by remember { mutableStateOf("Hello") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
 }
 
 @Composable
