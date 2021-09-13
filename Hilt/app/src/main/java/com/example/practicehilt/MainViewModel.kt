55aa4910cb83fab16d59e6632a56c8a70e194206
minjun8946 <minjun8946@daum.net>
2021-09-14 08:38:07 +0900

    ⚡️:: main viewmodel

--- Hilt/app/src/main/java/com/example/practicehilt/MainViewModel.kt
+++ Hilt/app/src/main/java/com/example/practicehilt/MainViewModel.kt
@@ -1,11 +1,21 @@
 package com.example.practicehilt
 
-import androidx.hilt.lifecycle.ViewModelInject
+import android.annotation.SuppressLint
 import com.example.practicehilt.base.BaseViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-
-class MainViewModel constructor() : BaseViewModel() {
-    fun a() = println("가나다라")
+@HiltViewModel
+class MainViewModel @Inject constructor(private val apiService: ApiService) : BaseViewModel() {
+    @SuppressLint("CheckResult")
+     fun team(){
+        val api = apiService.getTeamList()
+        api.observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe { response ->
+                println("${response.data} rkskek")
+            }
+    }
 }
