2f0577383842b60ebaf28e3294a5f63dbb1ce605
minjun8946 <minjun8946@daum.net>
2021-09-14 08:38:26 +0900

    ⚡️:: base activity

--- Hilt/app/src/main/java/com/example/practicehilt/base/BaseActivity.kt
+++ Hilt/app/src/main/java/com/example/practicehilt/base/BaseActivity.kt
@@ -6,3 +6,19 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.example.practicehilt.BR
 
+abstract class BaseActivity<T : ViewDataBinding, R : BaseViewModel> : AppCompatActivity() {
+    lateinit var binding: T
+
+    abstract val layoutId : Int
+    abstract val viewModel: R
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this@BaseActivity
+        binding.setVariable(BR.vm,viewModel)
+        init()
+    }
+    abstract fun init()
+}
