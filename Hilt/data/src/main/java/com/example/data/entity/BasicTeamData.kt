57ed56040de0cab4456a7237b65cb20137888723
minjun8946 <minjun8946@daum.net>
2021-09-14 10:34:46 +0900

    ⚡️:: basic team data

---
+++ Hilt/data/src/main/java/com/example/data/entity/BasicTeamData.kt
@@ -0,0 +1,25 @@
+package com.example.data.entity
+
+import com.example.domain.entity.BasicTeam
+import com.google.gson.annotations.SerializedName
+
+data class BasicTeamData(
+    @SerializedName("id")
+    val teamId : Int,
+    @SerializedName("name")
+    val teamName : String,
+    @SerializedName("city")
+    val city : String,
+    @SerializedName("conference")
+    val conference : String,
+    @SerializedName("division")
+    val division : String
+)
+fun BasicTeamData.toEntity() =
+    BasicTeam(
+        teamId = teamId,
+        teamName= teamName,
+        city = city,
+        conference = conference,
+        division = division
+    )
