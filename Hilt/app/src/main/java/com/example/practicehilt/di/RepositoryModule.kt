d7daec6a9ef27e433ccaf0c5ec29d4a85a465831
minjun8946 <minjun8946@daum.net>
2021-09-14 10:21:13 +0900

    ⚡️:: repository module

---
+++ Hilt/app/src/main/java/com/example/practicehilt/di/RepositoryModule.kt
@@ -0,0 +1,21 @@
+package com.example.practicehilt.di
+
+import com.example.data.datasource.DataSource
+import com.example.data.repository.RepositoryImpl
+import com.example.domain.repository.Repository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideRepository(
+        dataSource: DataSource
+    ) : Repository = RepositoryImpl(dataSource)
+}
