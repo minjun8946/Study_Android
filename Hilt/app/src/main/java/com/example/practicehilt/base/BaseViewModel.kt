aad7ace87bdacd4a1272889705f0e27f088587e6
minjun8946 <minjun8946@daum.net>
2021-09-14 10:30:41 +0900

    ⚡️:: viewmodel

--- Hilt/app/src/main/java/com/example/practicehilt/base/BaseViewModel.kt
+++ Hilt/app/src/main/java/com/example/practicehilt/base/BaseViewModel.kt
@@ -1,10 +1,36 @@
 package com.example.practicehilt.base
 
 import androidx.lifecycle.ViewModel
+import com.example.domain.base.Result
+import com.example.domain.base.UseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 open class BaseViewModel : ViewModel(){
 
+    private val compositeDisposable = CompositeDisposable()
+
+    private fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    fun <T,E>execute(
+        data: T, disposableSingleObserver: DisposableSingleObserver<E>,
+        useCase: UseCase<T, E>
+    ){
+        val disposable = useCase.create(data)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(disposableSingleObserver)
+
+        addDisposable(disposable)
+    }
+
     override fun onCleared() {
+        compositeDisposable.clear()
         super.onCleared()
     }
 }
