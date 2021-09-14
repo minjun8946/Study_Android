1d8e55670d8192e792c6b9933d5358404c027864
minjun8946 <minjun8946@daum.net>
2021-09-14 10:31:38 +0900

    ⚡️:: basicteam

---
+++ Hilt/domain/src/main/java/com/example/domain/entity/BasicTeam.kt
@@ -0,0 +1,9 @@
+package com.example.domain.entity
+
+data class BasicTeam(
+    val teamId : Int,
+    val teamName : String,
+    val city : String,
+    val conference : String,
+    val division : String
+)
