df3d334db00c5e7273280b8bbc9e6f30f60b665e
minjun8946 <minjun8946@daum.net>
2021-09-30 23:38:08 +0900

    🔥:: create simple compse project

---
+++ SimpleCompose/app/src/main/java/com/example/simplecompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.simplecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplecompose.ui.theme.SimpleComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SimpleComposeTheme {
+        Greeting("Android")
+    }
+}
